e6c448a2313bc450db136c760150e0bad79426e1
Arunkumar1712 <arunk05691@gail.com>
2024-03-24 10:58:00 +0530

    updatte

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
 import  dotenv from "dotenv";
 dotenv.config();
@@ -15,37 +15,37 @@ const MONGOURL = process.env.MONGO_URL;
 // Asigning DBName
 let DBName = "Roombookingtask";
 
-// const client = new MongoClient(MONGOURL);
+const client = new MongoClient(MONGOURL);
 let roomsdetails; // collection in db for rooms
 let Bookingrecords; //collection in db for Booking
 app.listen(port, () => console.log("Server Started"));
 // Function to conect MongoDB
-// async function createConnection() {
-//   try{
-//   await client.connect();
-//   const db = client.db(DBName);
-//   roomsdetails = db.collection("rooms");
-//   Bookingrecords = db.collection("Booking");
-//   console.log("mongodb is connected");
-//   }catch (error){
-//     console.error('Failed to connect to MongoDB:', error);
-//   }
-// }
-// createConnection(); //calling the function
+async function createConnection() {
+  try{
+  await client.connect();
+  const db = client.db(DBName);
+  roomsdetails = db.collection("rooms");
+  Bookingrecords = db.collection("Booking");
+  console.log("mongodb is connected");
+  }catch (error){
+    console.error('Failed to connect to MongoDB:', error);
+  }
+}
+createConnection(); //calling the function
 
 console.log(MONGOURL); // Log MongoDB connection string here
 
-mongoose.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  console.log("MongoDB is connected");
+// mongoose.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+//   console.log("MongoDB is connected");
 
-  // Accessing the database and collections
-  const db = mongoose.connection;
-  roomsdetails = db.collection("rooms"); // Access "rooms" collection
-  Bookingrecords = db.collection("Booking"); // Access "Booking" collection
+//   // Accessing the database and collections
+//   const db = mongoose.connection;
+//   roomsdetails = db.collection("rooms"); // Access "rooms" collection
+//   Bookingrecords = db.collection("Booking"); // Access "Booking" collection
 
-}).catch((error) => {
-  console.log("Error in connecting to MongoDB:", error);
-});
+// }).catch((error) => {
+//   console.log("Error in connecting to MongoDB:", error);
+// });
 
 
 
